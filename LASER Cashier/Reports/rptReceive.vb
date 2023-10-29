425211626d831bb416e223971583070d02c779ee
Maleesha Rodrigo <maleesha.rodrigo@car-care.se>
2023-10-29 12:35:16 +0530

    LASER Cashier - Fixed the Received Repair doesn't print issue

--- LASER Cashier/Reports/rptReceive.vb
+++ LASER Cashier/Reports/rptReceive.vb
@@ -78,7 +78,7 @@ Public Class rptReceive
     
     <Browsable(false),  _
      DesignerSerializationVisibilityAttribute(System.ComponentModel.DesignerSerializationVisibility.Hidden)>  _
-    Public ReadOnly Property DetailSection1() As CrystalDecisions.CrystalReports.Engine.Section
+    Public ReadOnly Property Section4() As CrystalDecisions.CrystalReports.Engine.Section
         Get
             Return Me.ReportDefinition.Sections(3)
         End Get
@@ -86,17 +86,9 @@ Public Class rptReceive
     
     <Browsable(false),  _
      DesignerSerializationVisibilityAttribute(System.ComponentModel.DesignerSerializationVisibility.Hidden)>  _
-    Public ReadOnly Property Section4() As CrystalDecisions.CrystalReports.Engine.Section
-        Get
-            Return Me.ReportDefinition.Sections(4)
-        End Get
-    End Property
-    
-    <Browsable(false),  _
-     DesignerSerializationVisibilityAttribute(System.ComponentModel.DesignerSerializationVisibility.Hidden)>  _
     Public ReadOnly Property Section5() As CrystalDecisions.CrystalReports.Engine.Section
         Get
-            Return Me.ReportDefinition.Sections(5)
+            Return Me.ReportDefinition.Sections(4)
         End Get
     End Property
     
