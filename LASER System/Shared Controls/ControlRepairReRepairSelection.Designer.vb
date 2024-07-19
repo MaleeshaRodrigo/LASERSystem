7dc592c35919d2dc1a0a69b477f7a360532c180a
Maleesha Rodrigo <w1902272@my.westminster.ac.uk>
2024-07-20 00:27:51 +0530

    #157: Implemented the ControlRepairReReapirSelection

--- LASER System/Shared Controls/ControlRepairReRepairSelection.Designer.vb
+++ LASER System/Shared Controls/ControlRepairReRepairSelection.Designer.vb
@@ -22,16 +22,74 @@ Partial Class ControlRepairReRepairSelection
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.ComboReRepNo = New System.Windows.Forms.ComboBox()
+        Me.ComboRepNo = New System.Windows.Forms.ComboBox()
+        Me.RadioReRepNo = New System.Windows.Forms.RadioButton()
+        Me.RadioRepNo = New System.Windows.Forms.RadioButton()
         Me.SuspendLayout()
+        '
+        'ComboReRepNo
+        '
+        Me.ComboReRepNo.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.ComboReRepNo.FormattingEnabled = True
+        Me.ComboReRepNo.Location = New System.Drawing.Point(102, 31)
+        Me.ComboReRepNo.Name = "ComboReRepNo"
+        Me.ComboReRepNo.Size = New System.Drawing.Size(92, 22)
+        Me.ComboReRepNo.TabIndex = 10
+        '
+        'ComboRepNo
+        '
+        Me.ComboRepNo.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.ComboRepNo.FormattingEnabled = True
+        Me.ComboRepNo.Location = New System.Drawing.Point(102, 3)
+        Me.ComboRepNo.Name = "ComboRepNo"
+        Me.ComboRepNo.Size = New System.Drawing.Size(92, 22)
+        Me.ComboRepNo.TabIndex = 9
+        '
+        'RadioReRepNo
+        '
+        Me.RadioReRepNo.AutoSize = True
+        Me.RadioReRepNo.Location = New System.Drawing.Point(3, 33)
+        Me.RadioReRepNo.Name = "RadioReRepNo"
+        Me.RadioReRepNo.Size = New System.Drawing.Size(93, 18)
+        Me.RadioReRepNo.TabIndex = 8
+        Me.RadioReRepNo.TabStop = True
+        Me.RadioReRepNo.Text = "Rerepair No:"
+        Me.RadioReRepNo.UseVisualStyleBackColor = True
+        '
+        'RadioRepNo
+        '
+        Me.RadioRepNo.AutoSize = True
+        Me.RadioRepNo.Location = New System.Drawing.Point(3, 5)
+        Me.RadioRepNo.Name = "RadioRepNo"
+        Me.RadioRepNo.Size = New System.Drawing.Size(82, 18)
+        Me.RadioRepNo.TabIndex = 7
+        Me.RadioRepNo.TabStop = True
+        Me.RadioRepNo.Text = "Repair No:"
+        Me.RadioRepNo.UseVisualStyleBackColor = True
         '
         'ControlRepairReRepairSelection
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 14.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.ComboReRepNo)
+        Me.Controls.Add(Me.ComboRepNo)
+        Me.Controls.Add(Me.RadioReRepNo)
+        Me.Controls.Add(Me.RadioRepNo)
+        Me.Font = New System.Drawing.Font("Calibri", 9.0!)
+        Me.MaximumSize = New System.Drawing.Size(0, 57)
+        Me.MinimumSize = New System.Drawing.Size(197, 57)
         Me.Name = "ControlRepairReRepairSelection"
-        Me.Size = New System.Drawing.Size(267, 156)
+        Me.Size = New System.Drawing.Size(197, 57)
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
+    Friend WithEvents ComboReRepNo As ComboBox
+    Friend WithEvents ComboRepNo As ComboBox
+    Friend WithEvents RadioReRepNo As RadioButton
+    Friend WithEvents RadioRepNo As RadioButton
 End Class
--- LASER System/Shared Controls/ControlRepairReRepairSelection.vb
+++ LASER System/Shared Controls/ControlRepairReRepairSelection.vb
@@ -1,3 +1,16 @@
 ﻿Public Class ControlRepairReRepairSelection
+    Private Db As Database
+
+    Public Sub SetDatabase(Db As Database)
+        Me.Db = Db
+    End Sub
+
+    Private Sub ComboRepNo_DropDown(sender As Object, e As EventArgs) Handles ComboRepNo.DropDown
+        ComboBoxDropDown(Db, ComboRepNo, "SELECT RepNo FROM Repair ORDER BY RepNo DESC;")
+    End Sub
+
+    Private Sub ComboReRepNo_DropDown(sender As Object, e As EventArgs) Handles ComboReRepNo.DropDown
+        ComboBoxDropDown(Db, ComboRepNo, "SELECT RetNo FROM `Return` ORDER BY RetNo DESC;")
+    End Sub
 
 End Class
