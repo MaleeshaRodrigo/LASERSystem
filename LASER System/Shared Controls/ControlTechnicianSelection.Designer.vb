db65ef857f9cd550dbc0fa038d8f859bac0fb1ae
Maleesha Rodrigo <w1902272@my.westminster.ac.uk>
2024-07-20 17:43:47 +0530

    #157: Updated the ControlTechnicianSelection

--- LASER System/Shared Controls/ControlTechnicianSelection.Designer.vb
+++ LASER System/Shared Controls/ControlTechnicianSelection.Designer.vb
@@ -22,8 +22,58 @@ Partial Class ControlTechnicianSelection
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.ComboTechnician = New System.Windows.Forms.ComboBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'ComboTechnician
+        '
+        Me.ComboTechnician.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.ComboTechnician.FormattingEnabled = True
+        Me.ComboTechnician.Location = New System.Drawing.Point(83, 3)
+        Me.ComboTechnician.Name = "ComboTechnician"
+        Me.ComboTechnician.Size = New System.Drawing.Size(119, 22)
+        Me.ComboTechnician.TabIndex = 5
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(9, 7)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(68, 14)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Technician:"
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Controls.Add(Me.ComboTechnician)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(205, 29)
+        Me.Panel1.TabIndex = 6
+        '
+        'ControlTechnicianSelection
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 14.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.Panel1)
+        Me.Font = New System.Drawing.Font("Calibri", 9.0!)
+        Me.MaximumSize = New System.Drawing.Size(0, 29)
+        Me.MinimumSize = New System.Drawing.Size(205, 29)
+        Me.Name = "ControlTechnicianSelection"
+        Me.Size = New System.Drawing.Size(205, 29)
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents ComboTechnician As ComboBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Panel1 As Panel
 End Class
