a619695504de3275301af3e0e5591059b4655f5c
Maleesha Rodrigo <w1902272@my.westminster.ac.uk>
2024-07-20 18:40:06 +0530

    #157: Implemented the ControlStockSelection

---
+++ LASER System/Shared Controls/ControlStockSelection.Designer.vb
@@ -0,0 +1,104 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class ControlStockSelection
+    Inherits System.Windows.Forms.UserControl
+
+    'UserControl overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.ComboStockName = New System.Windows.Forms.ComboBox()
+        Me.ComboStockCategory = New System.Windows.Forms.ComboBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.ComboStockName)
+        Me.Panel1.Controls.Add(Me.ComboStockCategory)
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(200, 60)
+        Me.Panel1.TabIndex = 0
+        '
+        'ComboStockName
+        '
+        Me.ComboStockName.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.ComboStockName.FormattingEnabled = True
+        Me.ComboStockName.Location = New System.Drawing.Point(69, 33)
+        Me.ComboStockName.Name = "ComboStockName"
+        Me.ComboStockName.Size = New System.Drawing.Size(127, 22)
+        Me.ComboStockName.TabIndex = 7
+        '
+        'ComboStockCategory
+        '
+        Me.ComboStockCategory.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.ComboStockCategory.FormattingEnabled = True
+        Me.ComboStockCategory.Location = New System.Drawing.Point(69, 3)
+        Me.ComboStockCategory.Name = "ComboStockCategory"
+        Me.ComboStockCategory.Size = New System.Drawing.Size(127, 22)
+        Me.ComboStockCategory.TabIndex = 6
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(7, 37)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(42, 14)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Name:"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(7, 7)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(56, 14)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Category:"
+        '
+        'ControlStockSelection
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 14.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.Panel1)
+        Me.Font = New System.Drawing.Font("Calibri", 9.0!)
+        Me.MaximumSize = New System.Drawing.Size(0, 60)
+        Me.MinimumSize = New System.Drawing.Size(200, 60)
+        Me.Name = "ControlStockSelection"
+        Me.Size = New System.Drawing.Size(200, 60)
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents ComboStockName As ComboBox
+    Friend WithEvents ComboStockCategory As ComboBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+End Class
---
+++ LASER System/Shared Controls/ControlStockSelection.vb
@@ -0,0 +1,35 @@
+﻿Imports MySqlConnector
+
+Public Class ControlStockSelection
+    Private Db As Database
+
+    Public Property SCategory()
+        Get
+            Return ComboStockCategory.Text
+        End Get
+        Set(value)
+            ComboStockCategory.Text = value
+        End Set
+    End Property
+
+    Public Property SName()
+        Get
+            Return ComboStockName.Text
+        End Get
+        Set(value)
+            ComboStockName.Text = value
+        End Set
+    End Property
+
+    Public Sub SetDatabase(Db As Database)
+        Me.Db = Db
+    End Sub
+
+    Private Sub ComboStockCategory_DropDown(sender As Object, e As EventArgs) Handles ComboStockCategory.DropDown
+        ComboBoxDropDown(Db, ComboStockCategory, "SELECT SCategory FROM Stock ORDER BY SCategory;")
+    End Sub
+
+    Private Sub ComboStockName_DropDown(sender As Object, e As EventArgs) Handles ComboStockName.DropDown
+        ComboBoxDropDown(Db, ComboStockName, $"SELECT SName FROM Stock WHERE SCategory='{ComboStockCategory.Text}' ORDER BY SName;")
+    End Sub
+End Class
