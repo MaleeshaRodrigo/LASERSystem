f680cd1f61b8fbd7c1c05ce18ba3393e03b785ed
Maleesha Rodrigo <101212851+MaleeshaRodrigo@users.noreply.github.com>
2024-08-12 00:16:39 +0530

    #170: Database Backup Process Introduced to LASER Background (#172)
    * #170: Renamed the FormBGTasks
    
    * #170: Implemented the database backup process
    
    * #170: Removed the BackUpDatabaseProcess
    
    * #170: Implemented the error class for database backup process
    
    * #170: LASER Background 2.1.2.0

--- LASER Background/Structures/ErrorClass.vb
+++ LASER Background/Structures/ErrorClass.vb
@@ -2,5 +2,6 @@
     Const AdminPermission = "AdminPermissionError"
     Const SendEmail = "LASER_Background.SendEmailProcess"
     Const SendSms = "LASER_Background.SendSmsProcess"
+    Const DatabaseBackup = "LASER_Background.DatabaseBackupProcess"
     Const Database = "DBError"
 End Structure
