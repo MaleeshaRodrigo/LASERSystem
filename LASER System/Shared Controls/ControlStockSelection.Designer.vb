cddbdb1f59dd42d1eb4d6c966b5e427b9ff9e01f
Maleesha Rodrigo <w1902272@my.westminster.ac.uk>
2024-08-24 16:35:07 +0530

    #171: Implemented the Stock code to ContorlStockSelection

--- LASER System/Shared Controls/ControlStockSelection.Designer.vb
+++ LASER System/Shared Controls/ControlStockSelection.Designer.vb
@@ -23,15 +23,20 @@ Partial Class ControlStockSelection
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.TextStockCode = New System.Windows.Forms.NumericUpDown()
+        Me.Label3 = New System.Windows.Forms.Label()
         Me.ComboStockName = New System.Windows.Forms.ComboBox()
         Me.ComboStockCategory = New System.Windows.Forms.ComboBox()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Panel1.SuspendLayout()
+        CType(Me.TextStockCode, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Panel1
         '
+        Me.Panel1.Controls.Add(Me.TextStockCode)
+        Me.Panel1.Controls.Add(Me.Label3)
         Me.Panel1.Controls.Add(Me.ComboStockName)
         Me.Panel1.Controls.Add(Me.ComboStockCategory)
         Me.Panel1.Controls.Add(Me.Label2)
@@ -39,15 +44,33 @@ Partial Class ControlStockSelection
         Me.Panel1.Dock = System.Windows.Forms.DockStyle.Fill
         Me.Panel1.Location = New System.Drawing.Point(0, 0)
         Me.Panel1.Name = "Panel1"
-        Me.Panel1.Size = New System.Drawing.Size(200, 60)
+        Me.Panel1.Size = New System.Drawing.Size(200, 90)
         Me.Panel1.TabIndex = 0
+        '
+        'TextStockCode
+        '
+        Me.TextStockCode.InterceptArrowKeys = False
+        Me.TextStockCode.Location = New System.Drawing.Point(69, 3)
+        Me.TextStockCode.Maximum = New Decimal(New Integer() {0, 0, 0, 0})
+        Me.TextStockCode.Name = "TextStockCode"
+        Me.TextStockCode.Size = New System.Drawing.Size(80, 22)
+        Me.TextStockCode.TabIndex = 9
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(7, 5)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(37, 14)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "Code:"
         '
         'ComboStockName
         '
         Me.ComboStockName.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
             Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.ComboStockName.FormattingEnabled = True
-        Me.ComboStockName.Location = New System.Drawing.Point(69, 33)
+        Me.ComboStockName.Location = New System.Drawing.Point(69, 61)
         Me.ComboStockName.Name = "ComboStockName"
         Me.ComboStockName.Size = New System.Drawing.Size(127, 22)
         Me.ComboStockName.TabIndex = 7
@@ -59,7 +82,7 @@ Partial Class ControlStockSelection
         Me.ComboStockCategory.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest
         Me.ComboStockCategory.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems
         Me.ComboStockCategory.FormattingEnabled = True
-        Me.ComboStockCategory.Location = New System.Drawing.Point(69, 3)
+        Me.ComboStockCategory.Location = New System.Drawing.Point(69, 31)
         Me.ComboStockCategory.Name = "ComboStockCategory"
         Me.ComboStockCategory.Size = New System.Drawing.Size(127, 22)
         Me.ComboStockCategory.TabIndex = 6
@@ -67,7 +90,7 @@ Partial Class ControlStockSelection
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(7, 37)
+        Me.Label2.Location = New System.Drawing.Point(7, 65)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(42, 14)
         Me.Label2.TabIndex = 5
@@ -76,7 +99,7 @@ Partial Class ControlStockSelection
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(7, 7)
+        Me.Label1.Location = New System.Drawing.Point(7, 35)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(56, 14)
         Me.Label1.TabIndex = 4
@@ -88,12 +111,13 @@ Partial Class ControlStockSelection
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.Controls.Add(Me.Panel1)
         Me.Font = New System.Drawing.Font("Calibri", 9.0!)
-        Me.MaximumSize = New System.Drawing.Size(0, 60)
-        Me.MinimumSize = New System.Drawing.Size(200, 60)
+        Me.MaximumSize = New System.Drawing.Size(0, 90)
+        Me.MinimumSize = New System.Drawing.Size(200, 90)
         Me.Name = "ControlStockSelection"
-        Me.Size = New System.Drawing.Size(200, 60)
+        Me.Size = New System.Drawing.Size(200, 90)
         Me.Panel1.ResumeLayout(False)
         Me.Panel1.PerformLayout()
+        CType(Me.TextStockCode, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
@@ -103,4 +127,6 @@ Partial Class ControlStockSelection
     Friend WithEvents ComboStockCategory As ComboBox
     Friend WithEvents Label2 As Label
     Friend WithEvents Label1 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextStockCode As NumericUpDown
 End Class
--- LASER System/Shared Controls/ControlStockSelection.vb
+++ LASER System/Shared Controls/ControlStockSelection.vb
@@ -6,25 +6,10 @@ Public Class ControlStockSelection
 
     Public Property SCode() As Integer
         Get
-            If ComboStockCategory.Text.Trim = "" Or ComboStockName.Text.Trim = "" Then
-                Return Nothing
-            End If
-
-            Return Db.GetData("SELECT SNo FROM Stock WHERE SCategory=@CATEGORY AND SName=@NAME", {
-                New MySqlParameter("CATEGORY", ComboStockCategory.Text),
-                New MySqlParameter("NAME", ComboStockName.Text)
-            })
+            Return TextStockCode.Value
         End Get
         Set(Value As Integer)
-            Dim Result = Db.GetDataDictionary("SELECT SCategory, SName FROM Stock WHERE SNo=@SNO;", {
-                New MySqlParameter("SNO", Value)
-            })
-            If Result Is Nothing Then
-                Exit Property
-            End If
-
-            ComboStockCategory.Text = Result(Stock.Category)
-            ComboStockName.Text = Result(Stock.Name)
+            TextStockCode.Value = Value
         End Set
     End Property
 
@@ -61,5 +46,28 @@ Public Class ControlStockSelection
     Private Sub ComboStockCategory_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboStockCategory.SelectedIndexChanged
         ComboBoxDropDown(Db, ComboStockName, $"SELECT SName FROM Stock WHERE SCategory='{ComboStockCategory.Text}' ORDER BY SName;")
         ComboStockName.Text = ""
+    End Sub
+
+    Private Sub TextStockCode_ValueChanged(sender As Object, e As EventArgs) Handles TextStockCode.ValueChanged
+        Dim Result = Db.GetDataDictionary("SELECT SCategory, SName FROM Stock WHERE SNo=@SNO;", {
+            New MySqlParameter("SNO", TextStockCode.Value)
+        })
+        If Result Is Nothing Then
+            Return
+        End If
+
+        ComboStockCategory.Text = Result(Stock.Category)
+        ComboStockName.Text = Result(Stock.Name)
+    End Sub
+
+    Private Sub ComboStockName_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboStockName.SelectedIndexChanged
+        If ComboStockCategory.Text.Trim = "" Or ComboStockName.Text.Trim = "" Then
+            Return
+        End If
+
+        TextStockCode.Value = Db.GetData("SELECT SNo FROM Stock WHERE SCategory=@CATEGORY AND SName=@NAME", {
+            New MySqlParameter("CATEGORY", ComboStockCategory.Text),
+            New MySqlParameter("NAME", ComboStockName.Text)
+        })
     End Sub
 End Class
