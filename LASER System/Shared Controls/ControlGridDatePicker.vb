d87d9cac74f655e20814b9539dee06184e7dafd4
Maleesha Rodrigo <w1902272@my.westminster.ac.uk>
2024-10-14 20:23:33 +0530

    #177: Implemented ControlGridDatePicker

---
+++ LASER System/Shared Controls/ControlGridDatePicker.vb
@@ -0,0 +1,24 @@
+﻿Public Class ControlGridDatePicker
+    Private Cell As DataGridViewCell
+
+    Public Property Value As Date
+        Get
+            Return DatePicker.Value
+        End Get
+        Set(Value As Date)
+            DatePicker.Value = Value
+        End Set
+    End Property
+
+    Public Sub Init(Cell As DataGridViewCell)
+        Me.Cell = Cell
+    End Sub
+
+    Private Sub DatePicker_ValueChanged(sender As Object, e As EventArgs) Handles DatePicker.ValueChanged
+        Cell.Value = DatePicker.Value
+    End Sub
+
+    Private Sub DatePicker_LostFocus(sender As Object, e As EventArgs) Handles DatePicker.LostFocus
+        Dispose()
+    End Sub
+End Class
