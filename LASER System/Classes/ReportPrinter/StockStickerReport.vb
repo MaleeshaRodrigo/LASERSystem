802e335f1b6b4205facff63a005f63d77a55aba2
Maleesha Rodrigo <101212851+MaleeshaRodrigo@users.noreply.github.com>
2025-10-04 18:32:50 +0530

    #249: When inserting a technician, only specific technician details would be saved (#250)
    * #249: Removed the BarcodeLib uses codes
    
    * #249: Fixed the technician insertion issue
    
    * #249: LASER System v2.7.0.3
    
    ---------
    
    Co-authored-by: Maleesha Rodrigo <maleeshar@99x.io>
    Co-authored-by: MaleeshaRodrigo <w1902272@westminster.ac.uk>

---
+++ LASER System/Classes/ReportPrinter/StockStickerReport.vb
@@ -0,0 +1,31 @@
+﻿Imports CrystalDecisions.CrystalReports.Engine
+
+Public Class StockStickerReport
+    Inherits AbstractReportManager
+    Implements ReportPrint
+
+    Public Function GenerateReport(Grid As DataGridView) As ReportClass
+        Dim Report As New rptStockSticker
+        Dim DataTable As New DataTable
+        DataTable.Clear()
+        DataTable.Columns.Add("SNo")
+        DataTable.Columns.Add("SCategory")
+        DataTable.Columns.Add("SName")
+        DataTable.Columns.Add("Qty")
+        DataTable.Columns.Add(New DataColumn("Barcode", GetType(Byte())))
+        DataTable.Columns.Add("Rate")
+        For Each row As DataGridViewRow In Grid.Rows
+            If row.Cells(0).Value Is Nothing Then
+                Exit For
+            End If
+
+            For i As Integer = 1 To row.Cells(4).Value.ToString
+                DataTable.Rows.Add(row.Cells.Item(0).Value, row.Cells.Item(1).Value, row.Cells.Item(2).Value, row.Cells.Item(4).Value, GetBarcode(row.Cells(0).Value), $"Rs.{row.Cells.Item(5).Value}")
+            Next
+        Next
+
+        Report.SetDataSource(DataTable)
+        Return Report
+    End Function
+
+End Class
